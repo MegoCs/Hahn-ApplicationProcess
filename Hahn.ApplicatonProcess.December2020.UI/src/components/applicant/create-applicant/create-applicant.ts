3263e0873a4e9ac1d24fdc2daf9fcfb55236acaf
Ahmed Magdy <MegoCs@Hotmail.com>
2021-01-09 09:53:18 +0200

    create applicant screen with applicant model

--- Hahn.ApplicatonProcess.December2020.UI/src/components/applicant/create-applicant/create-applicant.ts
+++ Hahn.ApplicatonProcess.December2020.UI/src/components/applicant/create-applicant/create-applicant.ts
@@ -1,7 +1,12 @@
+import {Applicant} from "../models/Applicant"
+
 export class CreateApplicant {
   message: string;
-
+  applicant:Applicant;
   constructor() {
     this.message = 'Hello world Applicant';
+  }
+  addApplicant(){
+    console.log(this.applicant);
   }
 }
